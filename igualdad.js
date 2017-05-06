77c4d368adb13f19db4130c1e539ea2eaf4b7d00
io <micorreo.erick@gmail.com>
2017-05-06 09:54:02 -0400

    ejemplos

--- igualdad.js
+++ igualdad.js
@@ -1,8 +1,8 @@
 var x = 5;
 var y = "5";
 
-console.log("x = 5");
-console.log("y = '5'");
+console.log("x = " + x);
+console.log("y = " + y);
 
 console.log("x == y: " + (x == y));
 console.log("x === y: " + (x === y));
