ac61db370ffa634ccd40c303d13a500d007dd32d
io <micorreo.erick@gmail.com>
2017-05-06 08:29:28 -0400

    typeof file

---
+++ typeof.js
@@ -0,0 +1,8 @@
+console.log("typeof 7: " + typeof 7);
+console.log("typeof hola: " + typeof "hola");
+console.log("typeof true: " + typeof true);
+console.log("typeof undefined: " + typeof undefined);
+console.log("typeof null: " + typeof null);
+console.log("typeof Date: " + typeof new Date());
+console.log("typeof Function: " + typeof new Function());
+console.log("typeof '13' + 7: " + typeof "13" + typeof + 7);
