f9f69b1f7ac73f01212799142ae095e1f67700a8
ioerick <micorreo.erick@gmail.com>
2017-05-20 09:32:22 -0400

    cambios lol

--- objeto.js
+++ objeto.js
@@ -1,2 +1,5 @@
-var pelicula={titulo:"xmen", genero:"accion", director:"marvel", duracion:150}
+var pelicula = {titulo:"xmen", genero:"accion", director:"marvel", duracion:150}
 console.log("pelicula: " + pelicula.titulo);
+
+var persona= {nombre:"pedro", apellido:"fernandez", edad:4}
+console.log("persona: " + persona.nombre + " " + persona.apellido + " " + persona.edad);
