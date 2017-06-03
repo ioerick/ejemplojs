6bef9df00f9a9164f6a4187082685bb60c0bc1dd
ioerick <micorreo.erick@gmail.com>
2017-06-03 09:51:43 -0400

    pff

--- proyectofinal/app.js
+++ proyectofinal/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials') //linea de la instancia de express-partials
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials()); //linea para el uso de express-partials en la app
 
 app.use('/', index);
 app.use('/users', users);
--- proyectofinal/routes/index.js
+++ proyectofinal/routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var objeto = {title:'algun titulo', visitante:'algun visitante', semestre:'algun semestre'};
+var reclamo = {tipoReclamo:'odeco', observacion:'mal servicio', fecha:'01/01/2017'};
+var objeto = {title:'algun titulo', visitante:'algun visitante', semestre:'algun semestre', reclamo:reclamo};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: objeto.title, visitante: objeto.visitante, semestre : objeto.semestre});
+  res.render('index', { title: objeto.title, visitante: objeto.visitante, semestre : objeto.semestre, reclamo:objeto.reclamo});
+});
+
+router.get('/registroReclamo', function(req, res) {
+  res.render('registroReclamo', {reclamo: reclamo });
 });
 
 module.exports = router;
